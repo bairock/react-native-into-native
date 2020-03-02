db91de0b3f37d433a81aae5e14783fc357980c85
Petr Vasilev <vpa17@ya.ru>
2020-03-02 23:16:05 +0900

    removed .idea folder from git

---
+++ ios/RNTest/NativeModule.m
@@ -0,0 +1,9 @@
+//
+//  NativeModule.m
+//  RNTest
+//
+//  Created by Petr Vasilev on 02.03.2020.
+//  Copyright © 2020 Julian Aiup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/RNTest/RNBridge.m
@@ -0,0 +1,9 @@
+//
+//  RNBridge.m
+//  RNTest
+//
+//  Created by Petr Vasilev on 02.03.2020.
+//  Copyright © 2020 Julian Aiup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
